605f08077db7fc31f1172650a3ed3f34ab93a80c
vainmoon <serzh.umnov.93@mail.ru>
2023-05-04 17:10:45 +0300

    Домашняя работа 7. Умнов Сергей Не проходят hard тесты

--- src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
+++ src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
@@ -5,55 +5,97 @@ import ru.mail.polis.homework.reflection.objects.easy.Easy;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
 /**
  * Необходимо реализовать метод reflectiveToString, который для произвольного объекта
  * возвращает его строковое описание в формате:
- * <p>
+ *
  * {field_1: value_1, field_2: value_2, ..., field_n: value_n}
- * <p>
+ *
  * где field_i - имя поля
  * value_i - его строковое представление (String.valueOf),
  * за исключением массивов, для которых value формируется как:
  * [element_1, element_2, ..., element_m]
  * где element_i - строковое представление элемента (String.valueOf)
  * элементы должны идти в том же порядке, что и в массиве.
- * <p>
+ *
  * Все null'ы следует представлять строкой "null".
- * <p>
+ *
  * Порядок полей
  * Сначала следует перечислить в алфавитном порядке поля, объявленные непосредственно в классе объекта,
  * потом в алфавитном порядке поля объявленные в родительском классе и так далее по иерархии наследования.
  * Примеры можно посмотреть в тестах.
- * <p>
+ *
  * Какие поля выводить
  * Необходимо включать только нестатические поля. Также нужно пропускать поля, помеченные аннотацией @SkipField
- * <p>
+ *
  * Упрощения
  * Чтобы не усложнять задание, предполагаем, что нет циклических ссылок, inner классов, и transient полей
- * <p>
+ *
  * Реализация
  * В пакете ru.mail.polis.homework.reflection можно редактировать только этот файл
  * или добавлять новые (не рекомендуется, т.к. решение вполне умещается тут в несколько методов).
  * Редактировать остальные файлы нельзя.
- * <p>
+ *
  * Баллы
  * В задании 3 уровня сложности, для каждого свой набор тестов:
  * Easy - простой класс, нет наследования, массивов, статических полей, аннотации SkipField (4 балла)
  * Easy + Medium - нет наследования, массивов, но есть статические поля и поля с аннотацией SkipField (6 баллов)
  * Easy + Medium + Hard - нужно реализовать все требования задания (10 баллов)
- * <p>
+ *
  * Итого, по заданию можно набрать 10 баллов
  * Баллы могут снижаться за неэффективный или неаккуратный код
  */
 public class ReflectionToStringHelper {
 
     public static String reflectiveToString(Object object) {
-        // TODO: implement
-        return null;
+        if (object == null) {
+            return "null";
+        }
+
+        Field[] fields = getAllFields(object.getClass());
+        TreeMap<String, Object> fieldMap = new TreeMap<>();
+
+        for (Field field : fields) {
+            if (!field.isAnnotationPresent(SkipField.class) && !java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+                try {
+                    field.setAccessible(true);
+                    Object value = field.get(object);
+                    if (value instanceof Object[]) {
+                        Object[] arrValue = (Object[]) value;
+                        Object[] strValue = new String[arrValue.length];
+                        for (int i = 0; i < arrValue.length; i++) {
+                            strValue[i] = String.valueOf(arrValue[i]);
+                        }
+                        value = Arrays.toString(strValue);
+                    } else {
+                        value = String.valueOf(value);
+                    }
+                    fieldMap.put(field.getName(), (value.equals("null")) ? null : value);
+                } catch (IllegalAccessException ignore) {
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        fieldMap.forEach((name, value) -> sb.append(name).append(": ").append(value).append(", "));
+        if (sb.length() > 1) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+        sb.append("}");
+
+        return sb.toString();
+    }
+
+    private static Field[] getAllFields(Class clazz) {
+        List<Field> fields = new ArrayList<>();
+        while (!clazz.equals(Object.class)) {
+            fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+            clazz = clazz.getSuperclass();
+        }
+        return fields.toArray(new Field[0]);
     }
 }
